fd75c62b747a27f2742651857cc059f4a8ce9cd4
PrabaHome <prababoy123@gmail.com>
2017-06-08 23:28:01 +0530

    new project

---
+++ Conso/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Conso
+{
+    class Program
+    {
+        public static int count = 0;
+        static void Main(string[] args)
+        {
+            var ttv = new Thread(countth);
+            var ttv1 = new Thread(countth);
+
+            ttv.Start();
+            Thread.Sleep(500);
+            ttv1.Start();
+        }
+
+        static void countth()
+        {
+            while (true)
+            {
+                count++;
+                Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " count " + count);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
--- ThreadTut/Program.cs
+++ ThreadTut/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadTut
@@ -10,6 +11,17 @@ namespace ThreadTut
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Main thread Id" + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 8; i++)
+            {
+                Thread test = new Thread(demoApplication);
+                test.Start();
+            }
+        }
+        public static void demoApplication()
+        {
+            while(true)
+            Console.WriteLine("test thread Id" + Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
